5f9d512df9530d2f169ab248c7b32ab2c25407f3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-10 08:13:30 -0700

    for 101-quote.c task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Program entry point
+ * program - to output to the standard error stderr
+ *
+ * Return: Will return 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
