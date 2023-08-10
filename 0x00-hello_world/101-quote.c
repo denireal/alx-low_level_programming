35dd3c2fdf1e8ee317c5c74694aecda95a10c156
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-10 08:26:51 -0700

    fix for 101-quote.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 /**
  * main - Program entry point
- * program - to output to the standard error stderr
+ * program - output to standard error stderr
  *
- * Return: Will return 1
+ * Return: return 1 (Failure)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	stdout);
 	return (1);
 }
