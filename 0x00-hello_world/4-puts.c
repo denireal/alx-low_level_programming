526e443088c71562a28ea0d9ce64892c03bb3ffa
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-10 06:20:40 -0700

    for 4-puts.c task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ * prints - "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
