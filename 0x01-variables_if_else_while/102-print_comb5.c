d883729d066d2985f7a2cf346325ddda7e78be73
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-14 05:38:47 -0700

    update for 102-print_comb5.c task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Program entry point
- * description - Program that prints all possible combinations
+ * description - This program prints all possible combinations
  * of two two-digit numbers
  *
  * Return: Always 0 (Success)
