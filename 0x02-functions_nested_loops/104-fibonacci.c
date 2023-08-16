e909aff43f7683816a5d58fc4805d4a2f9e2693d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-16 07:15:01 -0700

    for 104 fibonacci task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,41 +2,43 @@
 #define LARGEST 10000000000
 
 /**
- * main - Entry point
+ * main - Program entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    unsigned long int front_digit_1 = 0, back_digit_1 = 1, front_digit_2 = 0, back_digit_2 = 2;
-    unsigned long int carry1, carry2, carry3;
-    int count;
+	unsigned long int front_digit_1 = 0, back_digit_1 = 1;
+	unsigned long int front_digit_2 = 0, back_digit_2 = 2;
+	unsigned long int a, b, c;
+	int count;
 
-    printf("%lu, %lu, ", back_digit_1, back_digit_2);
-    for (count = 2; count < 98; count++)
-    {
-        if (back_digit_1 + back_digit_2 > LARGEST || front_digit_2 > 0 || front_digit_1 > 0)
-        {
-            carry1 = (back_digit_1 + back_digit_2) / LARGEST;
-            carry2 = (back_digit_1 + back_digit_2) % LARGEST;
-            carry3 = front_digit_1 + front_digit_2 + carry1;
-            front_digit_1 = front_digit_2;
-            front_digit_2 = carry3;
-            back_digit_1 = back_digit_2;
-            back_digit_2 = carry2;
-            printf("%lu%010lu", front_digit_2, back_digit_2);
-        }
-        else
-        {
-            carry2 = back_digit_1 + back_digit_2;
-            back_digit_1 = back_digit_2;
-            back_digit_2 = carry2;
-            printf("%lu", back_digit_2);
-        }
-        if (count != 97)
-            printf(", ");
-    }
-    printf("\n");
-    return (0);
+	printf("%lu, %lu, ", back_digit_1, back_digit_2);
+	for (count = 2; count < 98; count++)
+	{
+		if (back_digit_1 + back_digit_2 > LARGEST || front_digit_2 > 0 || front_digit_1 > 0)
+		{
+			a = (back_digit_1 + back_digit_2) / LARGEST;
+			b = (back_digit_1 + back_digit_2) % LARGEST;
+			c = front_digit_1 + front_digit_2 + a;
+			front_digit_1 = front_digit_2;
+			front_digit_2 = c;
+			back_digit_1 = back_digit_2;
+			back_digit_2 = b;
+
+			printf("%lu%010lu", front_digit_2, back_digit_2);
+		}
+		else
+		{
+			b = back_digit_1 + back_digit_2;
+			back_digit_1 = back_digit_2;
+			back_digit_2 = b;
+
+			printf("%lu", back_digit_2);
+		}
+		if (count != 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
 }
-
