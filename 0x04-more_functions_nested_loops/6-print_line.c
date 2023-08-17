089c48e4f50324f9f1bf4065ba558bbc1d65729d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-17 07:15:30 -0700

    for 6-print_line.c task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - program draws a straight line in the terminal.
+ * @line: the integer to be inputed
+ *
+ * Return: void
+ */
+void print_line(int line)
+{
+	int i = 0;
+
+	if (line > 0)
+	{
+		while (i < line)
+			_putchar('_');
+			i++
+	}
+	_putchar('\n');
+}
