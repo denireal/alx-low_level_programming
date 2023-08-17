7c01e884245a9c6dabd9847a538f55ef7374b90c
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-17 06:52:57 -0700

    for 2-mul.c task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - program ultiplies 2 numbers
+ * @num1: first number.
+ * @numb2: second number.
+ *
+ * Return: The sum (int).
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
