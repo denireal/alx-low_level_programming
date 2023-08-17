d3416483be590a26c6fc24328c3f9b697dc66035
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-17 07:22:37 -0700

    for 7-print_diagonal.c task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line.
+ * @n: n input integer
+ *
+ * Return: void
+ */void print_diagonal(int n)
+{
+	int prev, i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		prev = i;
+		while (prior > 0)
+		{
+			_putchar(32);
+			prev--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
