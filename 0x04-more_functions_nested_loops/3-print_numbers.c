fac43977f680038b4ac918b2a0054ad14d5799cd
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-17 06:59:37 -0700

    for 3-print_numbers.c task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
+
