749aaacfa4c73b848be89f0a163962e7b1284134
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-21 10:10:03 -0700

    for 2-strlentask

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,14 @@
-#include "main.h" 
- /** 
-  * _strlen - program returns the length of a string. 
-  * @s: variable pointer character 
-  * Return: string_len
-  */ 
+#include "main.h"
+/**
+*_strlen - program returns the length of a string
+*@s: variable pointer character
+*Return: string_len
+*/
 int _strlen(char *s)
 {
 	int string_len = 0;
- 
-	for (; s[string_len]; string_len++)
+
+	for (; s[string_len] != '\0'; string_len++)
 	{
 	}
 	string_len++;
