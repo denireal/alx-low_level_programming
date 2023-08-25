9d461cdacd0a81f488a9fdbcee830a26d326504c
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-25 15:23:55 -0700

    for 102-magic.c task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
 int main(void)
 {
 	int n;
@@ -18,5 +23,5 @@ int main(void)
 	p[5]  = 98;
 	/* ...so that this prints 98\n */
 	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+	return (0);
 }
