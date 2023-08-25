0bfb0fe67398504965bcbc919f1cb26e87dad878
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-25 10:40:52 -0700

    for 100-rot13.c task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* rot13 - function encodes a string
+* @s: the input string
+*
+* Return: returns an encoded string
+*/
+
+char *rot13(char *s)
+{
+char *ptr = s;
+char rot13_table[256];
+
+int i = 0;
+char c = 'a';
+
+for (; i < 256; i++)
+{
+rot13_table[i] = i;
+}
+
+for (; c <= 'z'; c++)
+{
+rot13_table[c] = 'a' + (c - 'a' + 13) % 26;
+rot13_table[c - 'a' + 'A'] = rot13_table[c] - 'a' + 'A';
+}
+
+while (*s)
+{
+*s = rot13_table[*s];
+s++;
+}
+
+return (ptr);
+}
