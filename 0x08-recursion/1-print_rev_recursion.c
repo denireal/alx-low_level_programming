7215f2c9ac14e9d6951c2e5ea381a8a3e6833050
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-31 02:37:06 -0700

    for 1-print_rev_recursion.c task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _print_rev_recursion - function prints a string in reverse
- * using recursion
- * @s: the input string to be printed
- *
- * Return: will return void
- */
+* _print_rev_recursion - function prints a string in reverse
+* using recursion
+* @s: the input string to be printed
+*
+* Return: will return void
+*/
 void _print_rev_recursion(char *s)
 {
-        if (*s)
-        {
-                _print_rev_recursion(s + 1);
-                _putchar(*s);
-        }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
