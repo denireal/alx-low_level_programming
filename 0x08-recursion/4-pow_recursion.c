29bc57b59a97723a7d42563aee64d4a92de2acb1
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-08-31 02:56:21 -0700

    for 4-pow_recursion.c task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print factorial of a number recursively
+ * @x: the base variavle
+ * @y: the exponent variable
+ *
+ * Return: the value of x raised y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+       return (x *= _pow_recursion(x, y - 1));
+}
