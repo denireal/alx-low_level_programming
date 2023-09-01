511615ef510a8df5d673156c684c8418c7b4d0b1
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-01 12:08:44 -0700

    for 2-args.c task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program's entry point. prints all the command-line
+ * arguments it receives, one per line
+ * @argc: integer representing the number of command line arguments
+ * @argv: An array of strings representing the command-line arguments.
+ * 
+ * Return: returns 0.
+ */
+int main(int argc, char **argv)
+{
+	int i = 0; /* Initialize a counter variable to 0 */
+
+	/* Use a while loop to iterate through the command-line arguments */
+	while (i < argc)
+	{
+	/* Print each command-line argument followed by a newline */
+		printf("%s\n", argv[i]);
+		i++; /* Increment the counter variable */
+	}
+
+	return (0); /* Return 0 to signify that the program executed successfully. */
+}
