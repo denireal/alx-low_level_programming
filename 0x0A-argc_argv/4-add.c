d7da77ea131118641fbaf368def8d7e64a931788
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-01 12:36:57 -0700

    for 4-add.c task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * is_valid_int - Check if a string represents a valid integer.
+ * @s: The string to check.
+ *
+ * Return: true if the string is a valid integer, false otherwise.
+ */
+
+bool is_valid_int(char *s);
+
+/**
+ * main - Entry point of the program. this function adds positive
+ * integers passed as command-line arguments.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: returns 0 if successful, 1 if an error occurred.
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int aggreg = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_valid_int(argv[i]))
+		{
+			aggreg += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", aggreg);
+	return (0);
+}
+
+bool is_valid_int(char *s)
+{
+	char *endptr;
+
+	long num = strtol(s, &endptr, 10);
+
+	if (*endptr != '\0' || num < INT_MIN || num > INT_MAX)
+	{
+		return (false);
+	}
+	return (true);
+}
