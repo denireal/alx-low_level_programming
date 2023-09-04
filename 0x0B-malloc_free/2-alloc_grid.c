5a20adfd5bb8097a108323ebd913f5d8d4b8d764
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-04 16:36:29 -0700

    for 3-alloc_grid.c task

---
+++ 0x0B-malloc_free/2-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * alloc_grid - program allocates a 2D grid of integers
+ * and initializes to 0.
+ * @width: width of the grid.
+ * @height: height of the grid.
+ *
+ * Return: returns a pointer to the allocated grid, or NULL on failure.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)calloc(width, sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
