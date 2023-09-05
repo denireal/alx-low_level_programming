afe44e710db79e296e8524e84792d5ed06e1a03f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-04 17:58:57 -0700

    for 100-argstostr.c task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -54,6 +54,10 @@ char *argstostr(int ac, char **av)
 		if (av[i])
 		{
 			concatenate_strings(combined_string, av[i], "\n");
+			if (i < ac - 1)
+			{
+				concatenate_strings(combined_string, "\n", "");
+			}
 		}
 	}
 	return (combined_string);
