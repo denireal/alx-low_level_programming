0cc92c5106fee06b014fa23e35f8bfd0d6fd85fb
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-07 11:54:28 -0700

    for 3-array_range.c task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Create an array of integers in a given range
+ * @min: The starting value of the range (inclusive)
+ * @max: The ending value of the range (inclusive)
+ *
+ * Return: A pointer to the dynamically allocated integer array
+ * or NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int i = 0, *a;
+
+	if (min > max)
+		return (NULL);
+	size_t size = (size_t)(max - min + 1);
+	a = malloc(sizeof(int) * size);
+
+	if (a == NULL)
+		return (NULL);
+	for (; i < size; i++)
+	{
+		a[i] = min++;
+	}
+	return (a);
+}
