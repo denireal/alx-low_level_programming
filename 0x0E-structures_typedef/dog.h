257c510524a48bc1bb8a855f66bf9757d8cb7865
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-11 07:20:31 -0700

    for dog.h task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+
+/* other task prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
