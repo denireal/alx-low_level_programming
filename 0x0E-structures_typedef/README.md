5a43bb6e78337fcdefb52c0a43ba89074d3a955c
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-11 07:19:21 -0700

    for dog.h task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+0x0E. C - Structures, typedef
+ By: Julien Barbier
+ Weight: 1
+ Project will start Sep 11, 2023 1:00 AM, must end by Sep 12, 2023 1:00 AM
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the struct called dog
+ * @name: pointer to a character array representing the name of the dog.
+ * @age: floating-point number representing the age of the dog.
+ * @owner: pointer to a character array representing the name of the dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+
+#endif
