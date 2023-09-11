7483d8d67598fde27ac9d681bf52f16449d4ae66
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-11 14:27:02 -0700

    for 2-print_dog.c task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.6f\n", d->age); /* Assuming a maximum precision of 6 decimal places for age */
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
