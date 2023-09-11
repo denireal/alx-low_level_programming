af1efd24db1d7a275e6980e8e3b7c9864a503a33
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-11 13:56:22 -0700

    for 4-sum.h task

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONLIKEMACRO_H
+#define FUNCTIONLIKEMACRO_H
+
+/**
+ * File: likemacro.h
+ * Desc: Header file defining the SUM macro.
+ * @x: First operand
+ * @y: Second operand
+ *
+ * The SUM macro calculates the sum of two values.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
