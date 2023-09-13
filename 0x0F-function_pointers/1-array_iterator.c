95bc3d33ec014fef9052302647db1e8894533d0d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-13 12:36:37 -0700

    for 1-array_iterator.c task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - apply an action to each element of an integer array. * @array: pointer to the integer array.
+ * array_iterator - apply an action to each element of an
+ * integer array
+ * @array: pointer to the integer array.
  * @size: size of the array.
- * @action: pointer to function that takes an integer as an argument.    */
+ * @action: pointer to function that takes an integer
+ * as an argument
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t ct = 0;
