caa46f054c6abbc4c9e2a63ddc1564722bb5dbf3
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-13 11:55:00 -0700

    for task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,10 +21,12 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 5)
 	{
-		if (!strcmp(ops[i].op, s))
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
