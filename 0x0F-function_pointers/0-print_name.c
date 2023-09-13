923efd7b6fba0e97cfaa99c89a08d4ef351ebfa7
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-13 06:38:43 -0700

    for 0-print_name.c task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,12 +8,12 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-        if (name == NULL || f == NULL)
-        {
-                return;
-        }
-        else
-        {
-                f(name);
-        }
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
 }
