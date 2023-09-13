1e9ed192b6b335a8fd56167c2d00ea1323899119
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-13 15:27:14 -0700

    fix for 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h task

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,38 +8,38 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-	char *operator;
+int num1, num2, result;
+char *operator;
 
-	if (argc != 4)
-	{
-		fprintf(stderr, "Error\n");
-		return (argc != 4 ? 98 : 0);
-	}
+if (argc != 4)
+{
+fprintf(stderr, "Error\n");
+return (argc != 4 ? 98 : 0);
+}
 
-	operator = argv[2];
+operator = argv[2];
 
-	if (strcmp(operator, "+") != 0 &&
-			strcmp(operator, "-") != 0 &&
-			strcmp(operator, "*") != 0 &&
-			strcmp(operator, "/") != 0 &&
-			strcmp(operator, "%") != 0)
-	{
-		fprintf(stderr, "Error\n");
-		return (99);
-	}
+if (strcmp(operator, "+") != 0 &&
+strcmp(operator, "-") != 0 &&
+strcmp(operator, "*") != 0 &&
+strcmp(operator, "/") != 0 &&
+strcmp(operator, "%") != 0)
+{
+fprintf(stderr, "Error\n");
+return (99);
+}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-	if ((*operator == '/' || *operator == '%') && num2 == 0)
-	{
-		fprintf(stderr, "Error\n");
-		return (100);
-	}
+if ((*operator == '/' || *operator == '%') && num2 == 0)
+{
+fprintf(stderr, "Error\n");
+return (100);
+}
 
-	result = (*get_op_func(operator))(num1, num2);
-	printf("%d\n", result);
+result = (*get_op_func(operator))(num1, num2);
+printf("%d\n", result);
 
-	return (0);
+return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -57,10 +57,13 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
+	int result;
+
+	while (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	return (a % b);
+	result = a % b;
+	return (result);
 }
