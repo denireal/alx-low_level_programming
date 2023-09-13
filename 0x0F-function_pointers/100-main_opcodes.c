b35ea0457a46b4eef8c30c3eaf462edf87cf62a2
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-13 14:16:39 -0700

    for 100-main_opcodes.c task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 
 /**
- * print_main_opcode - Prints the opcode of the main function.
- * @bytes: Number of bytes to print.
- */
-void print_main_opcode(int bytes)
-{
-	int i;
-	unsigned char *main_function;
-
-	main_function = (unsigned char *)print_main_opcode;
-
-	for (i = 0; i < bytes; i++)
-	{
-		printf("%02hhx ", main_function[i]);
-	}
-	printf("\n");
-}
-
-/**
  * main - Entry point.
  * @argc: Argument count.
  * @argv: Array of arguments.
@@ -28,25 +10,27 @@ void print_main_opcode(int bytes)
  */
 int main(int argc, char *argv[])
 {
-	int bytes;
+        int i, bytes;
+        unsigned char *mainfunc_ptr;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <number_of_bytes>\n", argv[0]);
-		return (1);
-	}
-	
-	bytes = atoi(argv[1]);
-	if (bytes <= 0)
-	{
-		fprintf(stderr, "Error: Number of bytes must be a positive integer.\n");
-		return (2);
-	}
-	if (bytes > 21)
-	{
-		fprintf(stderr, "Error: Number of bytes cannot exceed 21.\n");
-		return (2);
-	}
-	print_main_opcode(bytes);
-	return (0);
+        if (argc != 2)
+        {
+                printf("Error\n");
+                exit(1);
+        }
+        bytes = atoi(argv[1]);
+        if (bytes < 0)
+        {
+                printf("Error\n");
+                exit(2);
+        }
+        mainfunc_ptr = (unsigned char *)main;
+        i = 0;
+        if (bytes > 0)
+        {
+                while (i < (bytes - 1))
+                        printf("%02hhx ", mainfunc_ptr[i++]);
+                printf("%hhx\n", mainfunc_ptr[i]);
+        }
+        return (0);
 }
