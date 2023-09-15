bddc6a9822885f8e794384334d78a7a142b938ce
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-14 18:54:10 -0700

    for 3-print_all.c task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,112 +4,72 @@
 #include <string.h>
 
 /**
- * print_char - Prints a character
- * @arg: A character argument
- */
-void print_char(va_list arg)
-{
-char c = va_arg(arg, int);
-
-write(1, &c, 1);
-}
-
-/**
- * print_int - Prints an integer
- * @arg: An integer argument
- */
-void print_int(va_list arg)
-{
-int num;
-char buffer[32];
-
-num = va_arg(arg, int);
-snprintf(buffer, sizeof(buffer), "%d", num);
-write(1, buffer, strlen(buffer));
-}
-
-/**
- * print_float - Prints a float
- * @arg: A double argument
- */
-void print_float(va_list arg)
-{
-double num = va_arg(arg, double);
-char buffer[32];
-
-snprintf(buffer, sizeof(buffer), "%f", num);
-write(1, buffer, strlen(buffer));
-}
-
-/**
- * print_string - Prints a string (or "(nil)" if NULL)
- * @arg: A string argument
- */
-void print_string(va_list arg)
-{
-char *s = va_arg(arg, char *);
-char *output = s ? s : "(nil)";
-
-write(1, output, strlen(output));
-}
-
-/**
- * print_all - Prints various types of data based on a format string
- * @format: A string containing format specifiers
- * @...: Variable number of arguments corresponding to the
- * format specifiers
+ * print_all - function that prints anything.
+ * @format: a list of arguments.
  *
- * Description: This function prints data of different types based
- * on the format
- * string. The format string can contain the following specifiers:
- *   - 'c' for char
- *   - 'i' for integer
- *   - 'f' for float
- *   - 's' for string (if the string is NULL, "(nil)" is printed)
- *   - Any other character is ignored
+ * Return: no return available.
  */
-void print_all(const char *const format, ...)
+void print_all(const char * const format, ...)
 {
-char newline = '\n';
-char separator[3] = {0};
-const char *format_ptr = format;
+va_list valist;
+unsigned int a = 0, b, c = 0;
+char *str;
+const char _arg[] = "cifs";
 
-va_list args;
-va_start(args, format);
-
-while (*format_ptr)
+va_start(valist, format);
+while (format && format[a])
 {
-char ch = *format_ptr;
-
-switch (ch)
+b = 0;
+while (_arg[b])
+{
+if (format[a] == _arg[b] && c)
+{
+write(1, ", ", 2);
+break;
+}
+b++;
+}
+switch (format[a])
 {
 case 'c':
-snprintf(separator, sizeof(separator), "%s", ", ");
-print_char(args);
-break;
-case 'i':
-snprintf(separator, sizeof(separator), "%s", ", ");
-print_int(args);
-break;
-case 'f':
-snprintf(separator, sizeof(separator), "%s", ", ");
-print_float(args);
-break;
-case 's':
-snprintf(separator, sizeof(separator), "%s", ", ");
-print_string(args);
-break;
-}
-
-if (separator[0] != '\0')
 {
-write(1, separator, strlen(separator));
+char ch = va_arg(valist, int);
+write(1, &ch, 1);
+c = 1;
+break;
 }
-
-format_ptr++;
+case 'i':
+{
+int num = va_arg(valist, int);
+char buffer[32];
+int len = snprintf(buffer, sizeof(buffer), "%d", num);
+write(1, buffer, len);
+c = 1;
+break;
 }
+case 'f':
+{
+double num = va_arg(valist, double);
+char buffer[32];
+int len = snprintf(buffer, sizeof(buffer), "%f", num);
 
-va_end(args);
-
-write(1, &newline, 1);
+write(1, buffer, len);
+c = 1;
+break;
+}
+case 's':
+str = va_arg(valist, char *);
+c = 1;
+if (!str)
+{
+write(1, "(nil)", 5);
+break;
+}
+write(1, str, strlen(str));
+break;
+}
+a++;
+}
+write(1, "\n", 1);
+va_end(valist);
 }
