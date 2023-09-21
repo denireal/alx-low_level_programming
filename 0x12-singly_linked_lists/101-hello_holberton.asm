81257be7ecf0048a59a662d2cc1892b088712cbd
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-21 12:22:11 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
-section .data
-	format db "Hello, Holberton", 10, 0
-
-section .text
-global main
-extern printf
-
+          global    main
+          extern    printf
 main:
-	mov rdi, format
-	call printf
-	mov rax, 60
-	xor rdi, rdi
-	syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`, 0
