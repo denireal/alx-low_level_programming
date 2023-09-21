b4d2f5dac063de5046a1bae7bb14ed00a82139d2
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-21 11:48:15 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,17 +7,10 @@ void first(void) __attribute__ ((constructor));
  * first - Prints a message before the main function is executed
  *
  * Description: This function is executed automatically before the main
- * function is called. It writes a message to the standard output using the
- * write system call.
+ * function is called. It writes a message to the standard output
  */
 void first(void)
 {
-	int i = 0;
-
-	char message[] = "You're beat! and yet, you must allow,\n"
-	                 "I bear my house upon my back!\n";
-
-	for (; message[i] != '\0'; i++) {
-		putchar(message[i]);
-	}
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
