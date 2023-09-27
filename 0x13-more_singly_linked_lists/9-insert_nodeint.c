9d63af6d02a731318c3a5747febbb72b31e63fcb
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-27 07:43:51 -0700

    task done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,42 +9,37 @@
 * Return: Pointer to the new node, or NULL on failure.
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
-listint_t *new;
-listint_t *h;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-h = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
 
-if (idx != 0)
-{
-for (i = 0; i < idx - 1 && h != NULL; i++)
-{
-h = h->next;
-}
-}
+	new->n = n;
+	new->next = NULL;
 
-if (h == NULL && idx != 0)
-return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
 
-new->n = n;
-
-if (idx == 0)
-
-if (idx == 0)
-{
-new->next = *head;
-*head = new;
-}
-else
-{
-new->next = h->next;
-h->next = new;
-}
-
-return (new);
+	return (NULL);
 }
