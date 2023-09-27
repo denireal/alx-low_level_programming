3dbf9530e07a6ed689b73e84981d99e71270a62b
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-27 07:34:49 -0700

    update for lists.h

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
-* count_listint - counts the number of elements in a linked list
+* print_listint - counts the number of elements in a linked list
 * @h: linked list of type listint_t to count
 *
 * Return: number of nodes
 */
-size_t count_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 size_t num = 0;
 
