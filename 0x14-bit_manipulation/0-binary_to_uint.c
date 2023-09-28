6128e0a4cfa97465ccea1aab27cbc18596240308
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-28 05:30:09 -0700

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,13 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
+int i = 0;
 unsigned int result = 0;
 
 if (b == NULL)
 return (0);
 
-for (int i = 0; b[i] != '\0'; i++)
+for (; b[i] != '\0'; i++)
 {
 if (b[i] == '0' || b[i] == '1')
 {
