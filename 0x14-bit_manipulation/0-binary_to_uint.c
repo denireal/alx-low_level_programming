a0a841252c05ebf0456abc0cadb5300e28a17360
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-28 05:23:37 -0700

    first commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h> /* for NULL definition */
+
+/**
+* binary_to_uint - Converts a binary number to an unsigned int.
+* @b: A pointer to a string containing '0' and '1' characters.
+*
+* Return: The converted number, or 0 if there are invalid characters
+* in the string or if b is NULL.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result = 0;
+
+if (b == NULL)
+return (0);
+
+for (int i = 0; b[i] != '\0'; i++)
+{
+if (b[i] == '0' || b[i] == '1')
+{
+result = result * 2 + (b[i] - '0');
+}
+else
+{
+return (0); /* Return 0 if an invalid character is encountered. */
+}
+}
+
+return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,7 @@
+0x14. C - Bit manipulation
+C
+ By: Julien Barbier
+ Weight: 1
+ Project will start Sep 28, 2023 1:00 AM, must end by Sep 29, 2023 1:00 AM
+ Checker was released at Sep 28, 2023 7:00 AM
+ An auto review will be launched at the deadline
