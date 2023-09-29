510d44dfe3113fa3ad38227f157c7b9bb9684b4f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-09-29 05:03:19 -0700

    fix for header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,14 +4,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
+/*
+ * main.h
  * Description: Header file containing prototypes for all functions
  */
 
-int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+
+/**
+ * @param c The character to be printed
+ * @return On success, returns the character that was printed. On error, returns -1.
+ */
+int _putchar(char c);
+
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
