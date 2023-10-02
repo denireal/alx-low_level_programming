417e0ac73010b29393f80385bacc02b1e84b9125
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-02 10:55:42 -0700

    task fifth commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,64 +1,76 @@
 #include "main.h"
 
 /**
-* main - Performs the task of copying one file's content to another.
-* @argc: Number of arguments provided when running the program.
-* @argv: Array storing the command-line arguments.
-*
-* Return: Returns 0 upon successful execution, or
-* error codes 97, 98, 99, or 100 on failure.
+* display_error_and_exit - Display an error message and exit.
+* @error_code: The exit code to use.
+* @format: The error message format.
 */
-int main(int argc, char *argv[])
+void display_error_and_exit(int error_code, const char *format, ...)
 {
-int source_fd, dest_fd;
-ssize_t bytes_read, bytes_written;
+va_list args;
+va_start(args, format);
+vfprintf(stderr, format, args);
+va_end(args);
+exit(error_code);
+}
+
+/**
+* copy_file - Copy the content of one file to another.
+* @source_fd: Source file descriptor.
+* @dest_fd: Destination file descriptor.
+*/
+void copy_file(int source_fd, int dest_fd)
+{
 char buffer[BUFSIZ];
-
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-return (97);
-}
-
-source_fd = open(argv[1], O_RDONLY);
-if (source_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-return (98);
-}
-
-dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (dest_fd == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(source_fd);
-return (99);
-}
+ssize_t bytes_read, bytes_written;
 
 while ((bytes_read = read(source_fd, buffer, BUFSIZ)) > 0)
 {
 bytes_written = write(dest_fd, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(source_fd);
-close(dest_fd);
-return (99);
+display_error_and_exit(99, "Error: Can't write to file\n");
 }
 }
 
 if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(source_fd);
-close(dest_fd);
-return (98);
+display_error_and_exit(98, "Error: Can't read from file\n");
 }
+}
+
+/**
+* main - Entry point of the program.
+* @argc: Number of arguments.
+* @argv: Arguments vector.
+* Return: 0 on success, appropriate error code on failure.
+*/
+int main(int argc, char *argv[])
+{
+int source_fd, dest_fd;
+
+if (argc != 3)
+{
+display_error_and_exit(97, "Usage: cp file_from file_to\n");
+}
+
+source_fd = open(argv[1], O_RDONLY);
+if (source_fd == -1)
+{
+display_error_and_exit(98, "Error: Can't read from file %s\n", argv[1]);
+}
+
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (dest_fd == -1)
+{
+display_error_and_exit(99, "Error: Can't write to file %s\n", argv[2]);
+}
+
+copy_file(source_fd, dest_fd);
 
 if (close(source_fd) == -1 || close(dest_fd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
-return (100);
+display_error_and_exit(100, "Error: Can't close file descriptors\n");
 }
 
 return (0);
