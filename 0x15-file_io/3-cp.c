5e87a5175eb9dd518cfed9c3b692e5c9274f1178
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-02 10:40:51 -0700

    task fifth commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,52 +1,25 @@
 #include "main.h"
 
 /**
-* error_file - Checks whether files can be opened.
-* @file_from: File descriptor for source file.
-* @file_to: File descriptor for destination file.
-* @argv: The arguments vector.
-*
-* Return: 0 on success, 98 or 99 on failure.
-*/
-int error_file(int file_from, int file_to, char *argv[])
-{
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-return (98);
-}
-if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-return (99);
-}
-return (0);
-}
-
-/**
-* copy_file - Copies the contents of one file to another.
+* copy_file - Copies the content of one file to another.
 * @src_fd: Source file descriptor.
 * @dest_fd: Destination file descriptor.
-* @argv: Arguments vector.
 *
 * Return: 0 on success, -1 on failure.
 */
-int copy_file(int src_fd, int dest_fd, char *argv[])
+int copy_file(int src_fd, int dest_fd)
 {
-ssize_t nchars, nwr;
-char buf[1024];
+ssize_t bytes_read, bytes_written;
+char buffer[BUFSIZ];
 
-while ((nchars = read(src_fd, buf, sizeof(buf))) > 0)
+while ((bytes_read = read(src_fd, buffer, BUFSIZ)) > 0)
 {
-nwr = write(dest_fd, buf, nchars);
-if (nwr == -1)
-return (-1);
+bytes_written = write(dest_fd, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+return (-1); /* Copying error */
 }
 
-if (nchars == -1)
-return (-1);
-
-return (0);
+return ((bytes_read == 0) ? 0 : -1);
 }
 
 /**
@@ -58,43 +31,43 @@ return (0);
 */
 int main(int argc, char *argv[])
 {
-int file_from, file_to, err_close;
+int source_fd, dest_fd, close_result;
+int exit_code = 0;
 
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-return (97);
+return (97); /* Invalid arguments */
 }
 
-file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-
-int error_code = error_file(file_from, file_to, argv);
-if (error_code != 0)
-return (error_code);
-
-if (copy_file(file_from, file_to, argv) == -1)
+source_fd = open(argv[1], O_RDONLY);
+if (source_fd == -1)
 {
-err_close = close(file_from);
-if (err_close == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+return (98); /* Read error */
+}
 
-err_close = close(file_to);
-if (err_close == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-return (99);
-}
-err_close = close(file_from);
-if (err_close == -1)
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (dest_fd == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-return (100);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(source_fd);
+return (99); /* Write error */
 }
-err_close = close(file_to);
-if (err_close == -1)
+
+if (copy_file(source_fd, dest_fd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-return (100);
+dprintf(STDERR_FILENO, "Error: Can't copy file\n");
+close_result = -1;
 }
-return (0);
+else
+close_result = 0;
+
+if (close(source_fd) == -1 || close(dest_fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
+exit_code = 100;
+}
+
+return ((close_result == 0) ? exit_code : 99);
 }
