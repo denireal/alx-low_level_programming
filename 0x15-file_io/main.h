9e940ec8c8eb9ce1e9cab8d90780992fd572083b
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-02 09:07:44 -0700

    task third commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
