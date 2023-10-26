2e74aeb3b85a43d655eea7774abeb2147715b31a
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-26 14:59:43 -0700

    task done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,14 +11,16 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned int bit_count = 0;
+unsigned long int mask = 1;
 unsigned long int xor_result = (n ^ m);
 
-while (xor_result > 0)
+
+while (mask <= xor_result)
 {
-if (xor_result & 1)
+if (mask & xor_result)
 bit_count++;
-xor_result >>= 1;
+mask <<= 1;
 }
 
-return bit_count;
+return (bit_count);
 }
