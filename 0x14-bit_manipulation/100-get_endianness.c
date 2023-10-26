2cfab01de763c995606f2395386e7c432af67a5d
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-26 15:10:18 -0700

    task done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,7 +8,13 @@
 *
 * Return: 0 if the system is big-endian, 1 if it is little-endian.
 */
+
 int get_endianness(void)
 {
-return (*(char *)&(int){1} == 1);
+unsigned int check_value = 1;
+char *byte_pointer = (char *)&check_value;
+
+if (*byte_pointer)
+return (1);
+return (0);
 }
