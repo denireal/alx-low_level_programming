8d35b80b3638e43134150c3d8db9e30141b673a4
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-10-30 12:38:03 -0700

    2-append_text_to_file.c task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,30 +10,32 @@ include "main.h"
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, bytes_appended, len = 0;
+int file_desc, num_letters, write_result;
 
-if (filename == NULL)
+if (!filename)
 return (-1);
 
-if (text_content != NULL)
+file_desc = open(filename, O_WRONLY | O_APPEND);
+
+if (file_desc == -1)
+return (-1);
+
+if (text_content)
 {
-while (text_content[len])
-len++;
+for (num_letters = 0; text_content[num_letters]; num_letters++)
+;
+
+write_result = write(file_desc, text_content, num_letters);
+
+if (write_result == -1)
+{
+close(file_desc);
+return (-1);
+}
 }
 
-fd = open(filename, O_WRONLY | O_APPEND);
-
-if (fd == -1)
+if (close(file_desc) == -1)
 return (-1);
 
-bytes_appended = write(fd, text_content, len);
-
-if (bytes_appended == -1)
-{
-close(fd);
-return (-1);
-}
-
-close(fd);
 return (1);
 }
