d3ddbcd69ca769316a3c972717bc012f69582419
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-07 10:14:01 -0800

    tasks done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,17 +5,17 @@
 * @head: Pointer to the head of the list
 * Return: Number of elements printed
 */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t count = 0;
 const dlistint_t *current = h;
 
-while (h != NULL)
+while (current != NULL)
 {
 printf("%d\n", current->n);
 current = current->next;
 count++;
 }
+
 return (count);
 }
