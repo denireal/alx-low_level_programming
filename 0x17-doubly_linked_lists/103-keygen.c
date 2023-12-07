b45b6fd5ca235c1a207d8fdd5cd8fcd355cbf80f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-07 09:49:49 -0800

    tasks done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -10,20 +10,30 @@
 */
 int main(int argc, char *argv[])
 {
+/* Declarations */
 size_t index;
-char *c = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 char key[7];
-strcpy(key, "      ");
+size_t username_len;
+unsigned int sum, product;
+char max_char;
+unsigned int squared_sum, rand_seed;
+char loop_variable;
 
-size_t username_len = strlen(argv[1]);
-unsigned int sum = 0, product = 1;
+/* Initialization */
+strcpy(key, "      ");
 
 
 if (argc != 2)
 {
 printf("Correct usage: ./keygen5 username\n");
-return (1);
+return 1; /* Exit with an error code */
 }
+
+
+username_len = strlen(argv[1]);
+sum = 0;
+product = 1;
 
 for (index = 0; index < username_len; index++)
 {
@@ -31,28 +41,34 @@ sum += argv[1][index];
 product *= argv[1][index];
 }
 
-key[0] = c[(username_len ^ 59) & 63];
-key[1] = c[(sum ^ 79) & 63];
-key[2] = c[(product ^ 85) & 63];
 
-char max_char = argv[1][0];
+key[0] = charset[(username_len ^ 59) & 63];
+key[1] = charset[(sum ^ 79) & 63];
+key[2] = charset[(product ^ 85) & 63];
+
+
+max_char = argv[1][0];
 for (index = 0; index < username_len; index++)
 if (max_char <= argv[1][index])
 max_char = argv[1][index];
 
-srand(max_char ^ 14);
-key[3] = c[rand() & 63];
 
-unsigned int squared_sum = 0, rand_seed = 0;
+srand(max_char ^ 14);
+key[3] = charset[rand() & 63];
+
+squared_sum = 0;
+rand_seed = 0;
+
 for (index = 0; index < username_len; index++)
 squared_sum += argv[1][index] * argv[1][index];
 
-char loop_variable;
 for (loop_variable = 0; loop_variable < argv[1][0]; loop_variable++)
 rand_seed = rand();
 
-key[4] = c[(squared_sum ^ 239) & 63];
-key[5] = c[(rand_seed ^ 229) & 63];
+
+key[4] = charset[(squared_sum ^ 239) & 63];
+key[5] = charset[(rand_seed ^ 229) & 63];
+
 
 printf("%s\n", key);
 return (0);
