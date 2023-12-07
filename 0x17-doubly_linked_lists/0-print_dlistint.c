7bfe4cd1d3bdc88088e7e04c578e6018b97d3e8f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-07 10:53:40 -0800

    tasks done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 size_t count = 0;
 const dlistint_t *current = h;
 
-while (current != NULL)
+while (current)
 {
+++count;
 printf("%d\n", current->n);
 current = current->next;
-count++;
 }
-
 return (count);
 }
