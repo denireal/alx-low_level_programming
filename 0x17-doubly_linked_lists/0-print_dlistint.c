0b12acb34ad1e519bcdff8e35b6d24a4cee57ebd
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-07 10:05:49 -0800

    tasks done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,23 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a doubly linked list
- * @head: Pointer to the head of the list
- * Return: Number of elements printed
- */
+* print_dlistint - Prints all elements of a doubly linked list
+* @head: Pointer to the head of the list
+* Return: Number of elements printed
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *current = h;
+size_t count = 0;
+const dlistint_t *current = h;
 
-    while (current != NULL)
-    {
-        printf("%d ", current->n);
-        current = current->next;
-        count++;
-    }
-    printf("\n");
+while (current != NULL)
+{
+printf("%d\n", current->n);
+current = current->next;
+count++;
+}
 
-    return (count);
+printf("-> %lu elements\n", count);
+
+return (count);
 }
