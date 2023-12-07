e60b4b9912ef69693f79e6b8a25da24af7e2c01a
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-12-07 10:18:51 -0800

    tasks done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,10 +10,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new_node;
 
-if (head == NULL)
-return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
+
 if (new_node == NULL)
 return (NULL);
 
